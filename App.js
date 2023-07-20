b8d1051a63c64ca6f50612ea13d6e3ad04795ba3
Denilson Lemus <dlemus@harding.edu>
2023-07-19 22:10:49 -0600

    ✨ Set navigation stack

--- App.js
+++ App.js
@@ -1,15 +1,26 @@
 import { Provider } from "react-redux";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { store } from "./src/redux/store";
 import Home from "./src/views/Home";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
     <Provider store={store}>
       <NavigationContainer>
         <SafeAreaProvider>
-          <Home />
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Home"
+              component={Home}
+              options={{
+                headerShown: false,
+              }}
+            />
+          </Stack.Navigator>
         </SafeAreaProvider>
       </NavigationContainer>
     </Provider>
