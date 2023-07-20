36ef35122dbceca4023c2007db76eac49edc0c91
Denilson Lemus <dlemus@harding.edu>
2023-07-19 21:52:43 -0600

    ✨ Wrap in navigation container

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import { Provider } from "react-redux";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-
+import { NavigationContainer } from "@react-navigation/native";
 import { store } from "./src/redux/store";
 import Home from "./src/views/Home";
 
 export default function App() {
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-        <Home />
-      </SafeAreaProvider>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Home />
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
